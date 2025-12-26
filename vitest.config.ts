3b9528f9b7bf67aa3f0959a89ffd201d22c3999a
Josh Burroughs <josh@qhool.com>
2025-12-26 09:25:35 -0500

    Attempt to enable UI (not all features working)

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,14 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    ui: true,
+    api: true,
+    includeSource: ['src/**/*.ts', 'src/**/*.tsx'],
+    includeTaskLocation: true,
+    open: true,
     setupFiles: [],
     coverage: {
+      enabled: true,
       provider: 'v8',
       reporter: ['text', 'text-summary', 'html', 'lcov'],
       include: [
